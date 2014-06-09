fec48d8d8055c817548626d5e33cc4272f7bd513
raduj.lukasz@gmail.com <raduj.lukasz@gmail.com>
2014-06-09 23:45:00 +0200

    CIty representation.

---
+++ src/main/java/com/raduy/City.java
@@ -0,0 +1,43 @@
+package com.raduy;
+
+import com.google.common.base.*;
+
+/**
+ * @author Lukasz Raduj <raduj.lukasz@gmail.com>
+ */
+public class City {
+    private final int x;
+    private final int y;
+
+
+    public City(int x, int y) {
+        Preconditions.checkArgument(x > 0);
+        Preconditions.checkArgument(y > 0);
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("x", x)
+                .add("y", y)
+                .toString();
+    }
+
+    public double computeDistanceTo(City anotherCity) {
+        final int xDiff = Math.abs(anotherCity.getX() - this.getX());
+        final int yDiff = Math.abs(anotherCity.getY() - this.getY());
+
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+}
---
+++ src/test/java/CityTest.java
@@ -0,0 +1,46 @@
+import com.raduy.City;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Lukasz Raduj <raduj.lukasz@gmail.com>
+ */
+public class CityTest {
+    final int X_COORDINATE = 100;
+    final int Y_COORDINATE = 101;
+
+    private City instance;
+
+
+    @Before
+    public void setUp() throws Exception {
+        instance = new City(X_COORDINATE, Y_COORDINATE);
+    }
+
+    @Test
+    public void shouldCreateCityWithProperCoordinates() throws Exception {
+        assertThat(instance.getX()).isEqualTo(X_COORDINATE);
+
+        assertThat(instance.getY()).isEqualTo(Y_COORDINATE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenAndCoordsIsNegative() throws Exception {
+        instance = new City(1, -2);
+    }
+
+    @Test
+    public void shouldBeAbleToComputeDistanceToAnotherCity() throws Exception {
+        //given
+        instance = new City(1, 1);
+        final City anotherCity = new City(5, 4);
+
+        //when
+        double distance = instance.computeDistanceTo(anotherCity);
+
+        //then
+        assertThat(Double.compare(distance, 5)).isEqualTo(0);
+    }
+}
