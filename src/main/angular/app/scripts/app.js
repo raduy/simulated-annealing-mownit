5b688be194132b1a7d55f3676c7462c13c74c5c4
raduj.lukasz@gmail.com <raduj.lukasz@gmail.com>
2014-06-10 23:39:11 +0200

    Angular introduction to project.

---
+++ src/main/angular/app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('angularApp', [
+  'ngResource',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
