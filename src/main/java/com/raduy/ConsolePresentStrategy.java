53c1341e6aece6232c04fd0e6b08e1940f9b3fdc
raduj.lukasz@gmail.com <raduj.lukasz@gmail.com>
2014-06-10 20:17:36 +0200

    Result and result presentation strategy.

---
+++ src/main/java/com/raduy/ConsolePresentStrategy.java
@@ -0,0 +1,39 @@
+package com.raduy;
+
+import java.util.List;
+
+/**
+ * @author Lukasz Raduj <raduj.lukasz@gmail.com>
+ */
+public class ConsolePresentStrategy implements IPresentStrategy {
+
+    @Override
+    public void present(AnnealingResult result) {
+
+        System.out.println("Simulated Annealing for Traveling Salesman problem");
+
+        System.out.println("Initial solution:");
+        System.out.println("\tDistance: " + result.getInitialDistance());
+        System.out.print("\tTour:\n");
+
+        printTour(result.getInitialTour());
+
+        System.out.println("\nFinal solution:");
+        System.out.println("\tDistance: " + result.getFinalDistance());
+        System.out.print("\tTour:\n");
+
+
+        printTour(result.getFinalTour());
+
+
+    }
+
+    private void printTour(List<City> tour) {
+        for (int i = 0, size = tour.size(); i < size; i++) {
+            System.out.print("\t\t" + tour.get(i));
+            if (i != size - 1) {
+                System.out.println(" -> ");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/raduy/IPresentStrategy.java
@@ -0,0 +1,9 @@
+package com.raduy;
+
+/**
+ * @author Lukasz Raduj <raduj.lukasz@gmail.com>
+ */
+public interface IPresentStrategy {
+
+    void present(AnnealingResult annealingResult);
+}
